24bcdfe5533822b905e0b421a728c2ccc36c61f5
douglasmatosdev <douglasmatosdev@gmail.com>
2024-05-02 11:54:28 -0300

    compress image

---
+++ src/lib/calcImageSize.ts
@@ -0,0 +1,8 @@
+export function calcImageSize(image: string) {
+    let y = 1;
+    if (image.endsWith('==')) {
+        y = 2
+    }
+    const x_size = (image.length * (3 / 4)) - y
+    return Math.round(x_size / 1024)
+}
